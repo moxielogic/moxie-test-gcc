281b7518edde9827c68201b587a4530dbe960b2d
Anthony Green <green@moxielogic.com>
2020-05-09 06:40:49 -0400

    Use job, not pod

--- moxie-test-gcc.sh
+++ moxie-test-gcc.sh
@@ -6,10 +6,6 @@ cd /home/moxie
 export PATH=/opt/moxielogic/bin:$PATH
 export DEJAGNU=/home/moxie/site.exp
 
-yum -y update moxielogic-moxie-elf-newlib \
-              moxielogic-moxie-elf-binutils \
-              moxielogic-moxie-elf-gdb-sim
-
 SECRETS=`curl -H "X-Vault-Token: $VAULT_MOXIEDEV_TOKEN" -X GET https://vault-labdroid.apps.ocp.labdroid.net/v1/secret/moxiedev`
 echo $SECRETS | (umask 077 && jq -r .data.id_moxiedev_rsa > /tmp/id_rsa)
 git config --global user.email "bot@moxielogic.com"
